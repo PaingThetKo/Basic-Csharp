d3440494a01f0c8a2166c4d7a751fd3558a50696
Paing Thet Ko <lordpaingthetko@gmail.com>
2024-05-07 12:06:01 +0630

    update w3 classes

---
+++ w3 Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w3_Classes
+{
+    class Car
+    {
+        public string model = "Mustang";
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.model);
+
+            Console.ReadLine();
+        }
+
+        
+    }
+
+    
+}
