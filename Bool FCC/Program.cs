d399a35d69b409c0fc4fe69b9b3c08fff0cc941f
Paing Thet Ko <lordpaingthetko@gmail.com>
2024-05-08 09:03:23 +0630

    Updated 1

---
+++ Bool FCC/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bool_FCC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            int value = 0;
+
+            if (flag)
+            {
+                Console.WriteLine($"Inside the code block: {value}");
+            }
+
+            value = 10;
+            Console.WriteLine($"Outside the code block: {value}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ switch case FCC/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_case_FCC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int employeelvl = 200;
+            string employeeName = "John Smith";
+
+            string title = "";
+
+            switch(employeelvl)
+            {
+                case 100: title = "Junior Associate";
+                    break;
+
+                case 200: title = "Senior Associate";
+                    break;
+
+                case 300: title = "Manager";
+                    break;
+
+                case 400: title = "Senior Manager";
+                    break;
+
+                default: title = "Associate";
+                    break;
+            }
+
+            Console.WriteLine($"{employeeName} , {title}");
+
+            Console.ReadLine();
+        }
+    }
+}
